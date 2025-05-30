e2b096d963a633786e8d8ae54dcaddb4763abed3
Natan Maia <natanmaiaplays@outlook.com>
2025-05-30 17:02:03 -0300

    salvar switch 70 75

--- app/src/main/java/com/example/alcoolgasolinacompose/view/PriceCompareView.kt
+++ app/src/main/java/com/example/alcoolgasolinacompose/view/PriceCompareView.kt
@@ -1,10 +1,14 @@
 package com.example.alcoolgasolinacompose.view
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -12,6 +16,8 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -21,25 +27,45 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.alcoolgasolinacompose.ui.theme.AlcoolGasolinaComposeTheme
 
+private const val SHARED_PREF_NAME = "NAME"
+private const val KEY_USE_75 = "use75"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PriceCompareView(navController: NavController, savedInstanceState: Bundle?) {
-
     val modifier:Modifier = Modifier
+
+    val context = LocalContext.current;
+    val sharedPref: SharedPreferences = remember {
+        context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+    }
 
     var precoGasolina by remember { mutableStateOf("") }
     var precoAlcool by remember { mutableStateOf("") }
 
-    var deveEscolherGasolina by remember { mutableStateOf(false) }
+    var deveEscolherGasolina by remember {
+        mutableStateOf(false)
 
-    Column (modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+    }
+
+    var use75 by remember {
+        mutableStateOf(sharedPref.getBoolean(KEY_USE_75, false)) //false is default value
+    }
+
+
+
+    Column (
+        modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
         TextField(
             value = precoGasolina,
@@ -55,19 +81,45 @@ fun PriceCompareView(navController: NavController, savedInstanceState: Bundle?) 
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
         )
 
+        Row (
+//            modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Rendimento: " + if (use75) {"75%"} else {"70%"})
+            Switch(
+                checked = use75,
+                onCheckedChange = {
+                    use75 = it
+                    //salvar
+                    with (sharedPref.edit()) {
+                        putBoolean(KEY_USE_75, use75)
+                        apply()
+                    }
+                }
+            )
+        }
+
         Spacer(Modifier.height(16.dp))
 
-        Text(if (deveEscolherGasolina) {"GASOLINA"} else {"ALCOOL"})
+        Text(if (deveEscolherGasolina) {"MELHOR ESCOLHA: GASOLINA"} else {"MELHOR ESCOLHA: ALCOOL"})
 
         Button(
-            onClick = { deveEscolherGasolina = calcDeveEscolherGasolina(precoGasolina.toFloat(), precoAlcool.toFloat())}
+            onClick = {
+                deveEscolherGasolina = calcDeveEscolherGasolina(precoGasolina.toFloat(), precoAlcool.toFloat(), use75)
+
+
+            }
         ) {
             Text("Calcular o melhor combustível")
         }
     }
+
 }
 
 
-fun calcDeveEscolherGasolina(gasolina:Float, alcool:Float) : Boolean {
-    return alcool/gasolina > 0.7
+fun calcDeveEscolherGasolina(gasolina:Float, alcool:Float, use75: Boolean) : Boolean {
+    var rate = 0.7
+    if (use75) rate = 0.75
+    return alcool/gasolina > rate
 }
